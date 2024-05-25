5c19272e2ed116c275475b0f8b4b68736f8a78d0
JefferNg <jefferng11@gmail.com>
2024-05-25 13:39:11 -0700

    implemented tower tracking

---
+++ WearAndTear/Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    private GameObject currentTower;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTower != null)
+        {
+            Ray camray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(camray, out RaycastHit hitInfor, 100f))
+            {
+                currentTower.transform.position = hitInfor.point;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                currentTower = null;
+            }
+        }
+    }
+
+    public void SetTowerToPlace(GameObject tower)
+    {
+        currentTower = Instantiate(tower, Vector3.zero, Quaternion.identity);
+    }
+}
--- WearAndTear/Assets/Scripts/TowerTargeting.cs
+++ WearAndTear/Assets/Scripts/TowerTargeting.cs
@@ -1,8 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TowerTargeting : MonoBehaviour
 {
-    
+    [SerializeField] private float targetRange = 3f;
+    [SerializeField] private Transform rotatePoint;
+    [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private LayerMask enemyMask;
+
+    private Transform target;
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateToTarget();
+
+        if (!CheckTargetInRange())
+        {
+            target = null;
+        }
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+        
+    }
+
+    private void RotateToTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+        
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        rotatePoint.rotation = Quaternion.RotateTowards(rotatePoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckTargetInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetRange;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetRange);
+    }
 }
