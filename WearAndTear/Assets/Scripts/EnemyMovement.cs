5d5d9a59e39ce2deac74327943ed779b6a8b1047
JefferNg <jefferng11@gmail.com>
2024-05-24 20:26:12 -0700

    enemies face towards where they move

--- WearAndTear/Assets/Scripts/EnemyMovement.cs
+++ WearAndTear/Assets/Scripts/EnemyMovement.cs
@@ -19,6 +19,10 @@ public class EnemyMovement : MonoBehaviour
         // move character position
         transform.position = Vector2.MoveTowards(transform.position, waypoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
 
+        Vector3 direction = waypoints.waypoints[waypointIndex].position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
         // if character reaches a waypoint marker
         if (Vector2.Distance(transform.position, waypoints.waypoints[waypointIndex].position) < 0.001f)
         {
