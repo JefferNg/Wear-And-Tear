5ff94c4012ce735b10fc324dc06c7cd53a9112c2
JefferNg <jefferng11@gmail.com>
2024-05-26 12:04:13 -0700

    added main menu functionality

--- WearAndTear/Assets/Scripts/MainMenu.cs
+++ WearAndTear/Assets/Scripts/MainMenu.cs
@@ -5,13 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject creditsPage;
 
     private void Start()
     {
-        
+        mainMenu.SetActive(true);
+        creditsPage.SetActive(false);
     }
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenCredits()
+    {
+        mainMenu.SetActive(false);
+        creditsPage.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        mainMenu.SetActive(true);
+        creditsPage.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit game");
     }
 }
